fd8b07c181085eba6464a06ef9f548710e633250
BuffGRbrain <62837537+BuffGRbrain@users.noreply.github.com>
2026-04-17 10:47:25 -0500

    Following s4vitar course with personal modifications for learning

---
+++ htbmachines.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+#Colores
+
+# Colours
+greenColour="\\e[0;32m\\033[1m"
+endColour="\\033[0m\\e[0m"
+redColour="\\e[0;31m\\033[1m"
+blueColour="\\e[0;34m\\033[1m"
+yellowColour="\\e[0;33m\\033[1m"
+purpleColour="\\e[0;35m\\033[1m"
+turquoiseColour="\\e[0;36m\\033[1m"
+grayColour="\\e[0;37m\\033[1m"
+
+function ctrl_c(){
+	echo -e "\n\n ${redColour}[!] Saliendo.. \n"${endColour}; #COn las llaves separamos variables de texto con caracteres especiales tambien
+	exit 1; #Codigo error de salida
+}
+
+#Capture Control C para que no se rompa el script y se cierre bien 
+trap ctrl_c INT 
+
+#Global variables
+main_url="https://htbmachines.github.io/bundle.js"
+
+function helpPanel(){
+	echo -e "\n ${yellowColour} [+] ${endColour} ${grayColour} Use cases: ${endColour} \n"
+	echo -e "\t ${purpleColour} -n <MACHINENAME> ${endColour} ${grayColour}  Buscar por nombre de maquina, case sensitive ${endColour}"
+	echo -e "\t ${purpleColour} -u  ${endColour} ${grayColour} Actualizar el archivo de referencia ${endColour}"
+	echo -e "\t ${purpleColour} -h ${endColour} ${grayColour} Mostrar panel de ayuda ${endColour} \n "
+
+}
+
+function searchMachine(){
+	machineName="$1"
+
+	echo "$machineName"
+}
+
+function download_bundle(){
+#curl -s X GET https://htbmachines.github.io/bundle.js | js-beautify > $location/bundle.js 
+	 local location=$1
+	 curl -s -X GET $main_url  | js-beautify > $location/bundle.js 
+}
+
+function updatejsbundle(){
+
+	download_bundle /tmp/"$temp_location" ;	
+
+
+	if ! [[ -f "./bundle.js"  ]]; then #Checks if the file does not exist
+		
+		echo "File does not exist, downloading...";	
+		mv /tmp/"$temp_location"/bundle.js ./bundle.js;
+		sleep 1;
+	else 
+		echo "File is downloaded, checking for updates...";
+		sleep 2;
+		#File exists, so time to check for any updates
+		if [[ -z "$(diff --brief /tmp/"$temp_location"/bundle.js ./bundle.js  2>/dev/null)"  ]]; then 
+			echo "The file is up to date"
+		else 
+			mv /tmp/"$temp_location"/bundle.js ./bundle.js;
+			echo "File updated";
+			sleep 2;
+		fi
+	fi
+
+}
+
+#Indicadores
+declare -i parameter_counter=0 #entero declarado en 0
+declare location="$(pwd)"
+declare temp_location="htbmachines_bundle"
+mkdir -p "/tmp/$temp_location"
+#Menu de inputs viables
+#: para listar varios
+while getopts "n:h:u" arg; do
+	case $arg in
+		n) machineName=$OPTARG; let parameter_counter+=1;;
+		h) ;; #Indicamos la funcion a llamar
+		u) let parameter_counter+=2;;
+	esac
+done
+
+if [ $parameter_counter -eq 1 ]; then
+	searchMachine $machineName; #TODO escribir la funcion	
+elif [ $parameter_counter -eq 2  ]; then
+	updatejsbundle;
+else
+	helpPanel;
+fi
